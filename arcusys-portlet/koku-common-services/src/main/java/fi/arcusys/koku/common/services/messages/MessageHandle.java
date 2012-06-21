5fbacf89938563a9fcb461cba2a59a2a109330cf
Tapani Kiiskinen <tapani.kiiskinen@arcusys.fi>
2012-06-21 16:25:24 +0300

    KOKU-1281: Crude implementation of the reply indicator
    KOKULT-423. The indicator is a text column of true/false currently.

--- arcusys-portlet/koku-common-services/src/main/java/fi/arcusys/koku/common/services/messages/MessageHandle.java
+++ arcusys-portlet/koku-common-services/src/main/java/fi/arcusys/koku/common/services/messages/MessageHandle.java
@@ -37,11 +37,11 @@ import fi.arcusys.koku.kv.messageservice.User;
  * Jun 22, 2011
  */
 public class MessageHandle extends AbstractHandle implements MessageTasks {
-	
+
 	private static final Logger LOG = Logger.getLogger(MessageHandle.class);
-	
-	private static final OrderBy MSG_ORDER_BY_DATE; 
-	
+
+	private static final OrderBy MSG_ORDER_BY_DATE;
+
 	static {
 		/* sets the order type, default is ordering by created date descending */
 		// NOTE: OrderBy is not immutable object!
@@ -49,14 +49,14 @@ public class MessageHandle extends AbstractHandle implements MessageTasks {
 		MSG_ORDER_BY_DATE.setField(Fields.CREATED_DATE);
 		MSG_ORDER_BY_DATE.setType(Type.DESC);
 	}
-	
+
 	private final MessageService ms;
-	
+
 	public MessageHandle(MessageSource messageSource) {
 		super(messageSource);
 		ms = new MessageService();
 	}
-	
+
 	@Override
 	public ResultList<KokuMessage> getRecievedMessages(final String uid,
 			final MessageSearchCriteria criteria, final Page page)
@@ -84,26 +84,26 @@ public class MessageHandle extends AbstractHandle implements MessageTasks {
 			throws KokuCommonException {
 		return getMessages(uid, FolderType.ARCHIVE_OUTBOX, criteria, page);
 	}
-		
+
 	private ResultList<KokuMessage> getMessages(final String uid, final FolderType folderType,
-			final MessageSearchCriteria criteria, final Page page) throws KokuCommonException {		
+			final MessageSearchCriteria criteria, final Page page) throws KokuCommonException {
 		final MessageQuery messageQuery = new MessageQuery();
 		messageQuery.setStartNum(page.getFirst());
 		messageQuery.setMaxNum(page.getLast());
 		messageQuery.setCriteria(convertMessageSearchToWsCriteria(criteria));
 		messageQuery.getOrderBy().add(MSG_ORDER_BY_DATE);
-		
+
 		final List<MessageSummary> msgs = ms.getMessages(uid, folderType, messageQuery);
 		final List<KokuMessage> msgList = new ArrayList<KokuMessage>();
 		for (MessageSummary msgSum : msgs) {
 			final KokuMessage message = new KokuMessage();
 			convertMessageSummaryToKokuMessage(message, msgSum);
-			msgList.add(message);		
+			msgList.add(message);
 		}
-		final int tasksTotal = getTotalMessageNum(uid, folderType, criteria); 
+		final int tasksTotal = getTotalMessageNum(uid, folderType, criteria);
 		return new ResultListImpl<KokuMessage>(msgList, tasksTotal, page);
 	}
-	
+
 	private Criteria convertMessageSearchToWsCriteria(final MessageSearchCriteria msgCriteria) {
 		final Criteria wsCriteria = new Criteria();
 		wsCriteria.getKeywords().addAll(msgCriteria.getKeywords());
@@ -116,14 +116,14 @@ public class MessageHandle extends AbstractHandle implements MessageTasks {
 		}
 		return wsCriteria;
 	}
-	
+
 	private int getTotalMessageNum(final String userId, final FolderType folderType, final MessageSearchCriteria criteria) throws KokuServiceException {
 		return ms.getTotalMessageNum(userId, folderType, convertMessageSearchToWsCriteria(criteria));
 	}
-	
+
 	/**
 	 * Returns the total amount of messages with given filtering conditions
-	 * 
+	 *
 	 * @param user username
 	 * @param messageType type of message
 	 * @param keyword keyword string for filtering
@@ -135,12 +135,12 @@ public class MessageHandle extends AbstractHandle implements MessageTasks {
 		FolderType folderType = MessageUtil.getFolderType(messageType);
 		/* sets the criteria for searching including keyword for each field, default is searching all fields */
 		Criteria criteria = createCriteria(keyword, field);
-		return ms.getTotalMessageNum(userId, folderType, criteria);		
+		return ms.getTotalMessageNum(userId, folderType, criteria);
 	}
-	
+
 	/**
 	 * Returns total amount of unread messages
-	 * 
+	 *
 	 * @param username
 	 * @param folderType
 	 * @return
@@ -150,28 +150,28 @@ public class MessageHandle extends AbstractHandle implements MessageTasks {
 		FolderType type = FolderType.valueOf(folderType.toString());
 		return ms.getUnreadMessageNum(username, type);
 	}
-	
+
 	/**
 	 * Gets detailed message with content
-	 * 
+	 *
 	 * @param messageId message id
 	 * @return detailed message
 	 */
 	public KokuMessage getMessageById(String messageId) throws KokuServiceException {
 		long  msgId = 0;
 		try {
-			 msgId = (long) Long.parseLong(messageId);			
+			 msgId = (long) Long.parseLong(messageId);
 		} catch (NumberFormatException nfe) {
 			LOG.error("Couldn't show message details, because messageId can't parse properly (must be long). Given MessageId: "+messageId, nfe);
 			return new KokuMessage();
 		}
-		setMessageStatusRead(msgId);		
+		setMessageStatusRead(msgId);
 		Message msg = ms.getMessageById(msgId);
 		KokuMessage message = new KokuMessage();
 		convertMessageToKokuMessage(message, msg);
 		return message;
 	}
-	
+
 	private void convertMessageSummaryToKokuMessage(final KokuMessage kokuMessage, final MessageSummary message) {
 		kokuMessage.setMessageId(message.getMessageId());
 		kokuMessage.setSubject(message.getSubject());
@@ -180,31 +180,32 @@ public class MessageHandle extends AbstractHandle implements MessageTasks {
 		kokuMessage.setMessageStatus(message.getMessageStatus().value());
 		kokuMessage.setMessageStatusLocalized(localizeMsgStatus(message.getMessageStatus()));
 		kokuMessage.setReplyDisabled(message.isReplyDisabled());
+		kokuMessage.setReplied(message.isReplied());
 		for (User recipientUser : message.getRecipientUserInfos()) {
 			kokuMessage.getRecipientUsers().add(new KokuUser(recipientUser));
 		}
 		kokuMessage.setSenderUser(new KokuUser(message.getSenderUserInfo()));
 	}
-	
+
 	private void convertMessageToKokuMessage(final KokuMessage kokuMessge, final Message message) {
 		convertMessageSummaryToKokuMessage(kokuMessge, message);
 		if (message.getDeliveryFailedTo() != null) {
 			for (User user : message.getDeliveryFailedTo()) {
-				kokuMessge.getDeliveryFailedTo().add(new KokuUser(user));				
+				kokuMessge.getDeliveryFailedTo().add(new KokuUser(user));
 			}
 		}
 		kokuMessge.setContent(message.getContent());
 	}
-	
-	
+
+
 	private String localizeMsgStatus(final MessageStatus status ) {
 		if (getMessageSource() == null) {
 			LOG.warn("getMessageSource() is null. Localization doesn't work properly");
 			return status.toString().toLowerCase();
 		}
 		Locale locale = MessageUtil.getLocale();
-		
-		try {	
+
+		try {
 			switch(status) {
 			case READ:
 				return getMessageSource().getMessage("MessageStatus.READ", null, locale);
@@ -218,72 +219,72 @@ public class MessageHandle extends AbstractHandle implements MessageTasks {
 			return status.toString().toLowerCase();
 		}
 	}
-	
+
 	/**
 	 * Sets message status to read
-	 * 
+	 *
 	 * @param messageId message id
-	 * @throws KokuServiceException 
+	 * @throws KokuServiceException
 	 */
 	private void setMessageStatusRead(long messageId) throws KokuServiceException {
-		List<Long> messageIds = new ArrayList<Long>();	
-		messageIds.add(messageId);	
+		List<Long> messageIds = new ArrayList<Long>();
+		messageIds.add(messageId);
 		ms.setMessageStatus(messageIds, MessageStatus.READ);
 	}
-	
+
 	/**
 	 * Archives messages
-	 * 
+	 *
 	 * @param messageIds a list of message ids
 	 * @return operation response information
-	 * @throws KokuServiceException 
+	 * @throws KokuServiceException
 	 */
-	public void archiveMessages(List<Long> messageIds) throws KokuServiceException {		
-		ms.archiveMessages(messageIds);		
+	public void archiveMessages(List<Long> messageIds) throws KokuServiceException {
+		ms.archiveMessages(messageIds);
 	}
-	
+
 	/**
 	 * Archives old messages
-	 * 
+	 *
 	 * @param userUid
 	 * @param folderType
 	 * @return operation response information
-	 * @throws KokuServiceException 
+	 * @throws KokuServiceException
 	 */
 	public void archiveOldMessages(String userUid, FolderType folderType) throws KokuServiceException {
 		ms.archiveOldMessages(userUid, folderType);
 	}
-	
+
 	/**
 	 * Deletes messages
-	 * 
+	 *
 	 * @param messageIds a list of message ids
 	 * @return operation response information
-	 * @throws KokuServiceException 
+	 * @throws KokuServiceException
 	 */
-	public void deleteMessages(List<Long> messageIds) throws KokuServiceException {				
-		ms.deleteMessages(messageIds);		
+	public void deleteMessages(List<Long> messageIds) throws KokuServiceException {
+		ms.deleteMessages(messageIds);
 	}
-	
+
 	/**
 	 * Creates filtering criteria
-	 * 
+	 *
 	 * @param keyword keyword string
 	 * @param field filed string
 	 * @return filtering criteria
 	 */
 	private Criteria createCriteria(String keyword, String field) {
 		Criteria criteria = new Criteria();
-		
+
 		if(keyword.trim().length() > 0) {
 			String[] keywords = keyword.split(" ");
 			criteria.getKeywords().addAll(Arrays.asList(keywords));
 		}else {
 			return null;
 		}
-		
+
 		String[] fields = field.split("_");
-		
+
 		for(int i=0; i < fields.length; i++) {
 			if(fields[i].equals("1")) {
 				criteria.getFields().add(Fields.SENDER);
--- arcusys-portlet/koku-common-services/src/main/java/fi/arcusys/koku/common/services/messages/model/KokuMessage.java
+++ arcusys-portlet/koku-common-services/src/main/java/fi/arcusys/koku/common/services/messages/model/KokuMessage.java
@@ -11,19 +11,20 @@ import fi.arcusys.koku.common.services.users.KokuUser;
  * Jun 22, 2011
  */
 public class KokuMessage {
-	
+
 	private long messageId;
 	private KokuUser senderUser;
-	private List<KokuUser> recipientUsers;	
+	private List<KokuUser> recipientUsers;
 	private String subject;
-	private String content;	
+	private String content;
 	private String creationDate;
 	private String messageType;
 	private String messageStatus;
 	private String messageStatusLocalized;
+	private boolean replied;
 	private boolean replyDisabled;
 	private List<KokuUser> deliveryFailedTo;
-	
+
 	/**
 	 * @return the senderUser
 	 */
@@ -51,51 +52,51 @@ public class KokuMessage {
 	public long getMessageId() {
 		return messageId;
 	}
-		
+
 	public String getSubject() {
 		return subject;
 	}
-	
+
 	public String getContent() {
 		return content;
 	}
-	
+
 	public String getCreationDate() {
 		return creationDate;
 	}
-	
+
 	public String getMessageType() {
 		return messageType;
 	}
-	
+
 	public String getMessageStatus() {
 		return messageStatus;
 	}
-	
+
 	public void setMessageId(long messageId) {
 		this.messageId = messageId;
 	}
-		
+
 	public void setSubject(String subject) {
 		this.subject = subject;
 	}
-	
+
 	public void setContent(String content) {
 		this.content = content;
 	}
-	
+
 	public void setCreationDate(String creationDate) {
 		this.creationDate = creationDate;
 	}
-	
+
 	public void setMessageType(String messageType) {
 		this.messageType = messageType;
 	}
-	
+
 	public void setMessageStatus(String messageStatus) {
 		this.messageStatus = messageStatus;
 	}
-	
+
 	/**
 	 * @return the messageStatusLocalized
 	 */
@@ -108,6 +109,14 @@ public class KokuMessage {
 	 */
 	public final void setMessageStatusLocalized(String messageStatusLocalized) {
 		this.messageStatusLocalized = messageStatusLocalized;
+	}
+
+	public boolean isReplied() {
+		return replied;
+	}
+
+	public void setReplied(boolean replied) {
+		this.replied = replied;
 	}
 
 	public boolean isReplyDisabled() {
@@ -130,9 +139,6 @@ public class KokuMessage {
 		return deliveryFailedTo;
 	}
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
 	@Override
 	public String toString() {
 		return "KokuMessage [messageId=" + messageId + ", senderUser="
@@ -140,12 +146,11 @@ public class KokuMessage {
 				+ ", subject=" + subject + ", content=" + content
 				+ ", creationDate=" + creationDate + ", messageType="
 				+ messageType + ", messageStatus=" + messageStatus
+				+ ", messageStatusLocalized=" + messageStatusLocalized
+				+ ", replied=" + replied + ", replyDisabled=" + replyDisabled
 				+ ", deliveryFailedTo=" + deliveryFailedTo + "]";
 	}
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#hashCode()
-	 */
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -159,90 +164,82 @@ public class KokuMessage {
 		result = prime * result + (int) (messageId ^ (messageId >>> 32));
 		result = prime * result
 				+ ((messageStatus == null) ? 0 : messageStatus.hashCode());
+		result = prime
+				* result
+				+ ((messageStatusLocalized == null) ? 0
+						: messageStatusLocalized.hashCode());
 		result = prime * result
 				+ ((messageType == null) ? 0 : messageType.hashCode());
 		result = prime * result
 				+ ((recipientUsers == null) ? 0 : recipientUsers.hashCode());
+		result = prime * result + (replied ? 1231 : 1237);
+		result = prime * result + (replyDisabled ? 1231 : 1237);
 		result = prime * result
 				+ ((senderUser == null) ? 0 : senderUser.hashCode());
 		result = prime * result + ((subject == null) ? 0 : subject.hashCode());
 		return result;
 	}
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj) {
+		if (this == obj)
 			return true;
-		}
-		if (obj == null) {
+		if (obj == null)
 			return false;
-		}
-		if (getClass() != obj.getClass()) {
+		if (getClass() != obj.getClass())
 			return false;
-		}
 		KokuMessage other = (KokuMessage) obj;
 		if (content == null) {
-			if (other.content != null) {
+			if (other.content != null)
 				return false;
-			}
-		} else if (!content.equals(other.content)) {
+		} else if (!content.equals(other.content))
 			return false;
-		}
 		if (creationDate == null) {
-			if (other.creationDate != null) {
+			if (other.creationDate != null)
 				return false;
-			}
-		} else if (!creationDate.equals(other.creationDate)) {
+		} else if (!creationDate.equals(other.creationDate))
 			return false;
-		}
 		if (deliveryFailedTo == null) {
-			if (other.deliveryFailedTo != null) {
+			if (other.deliveryFailedTo != null)
 				return false;
-			}
-		} else if (!deliveryFailedTo.equals(other.deliveryFailedTo)) {
+		} else if (!deliveryFailedTo.equals(other.deliveryFailedTo))
 			return false;
-		}
-		if (messageId != other.messageId) {
+		if (messageId != other.messageId)
 			return false;
-		}
 		if (messageStatus == null) {
-			if (other.messageStatus != null) {
+			if (other.messageStatus != null)
 				return false;
-			}
-		} else if (!messageStatus.equals(other.messageStatus)) {
+		} else if (!messageStatus.equals(other.messageStatus))
 			return false;
-		}
+		if (messageStatusLocalized == null) {
+			if (other.messageStatusLocalized != null)
+				return false;
+		} else if (!messageStatusLocalized.equals(other.messageStatusLocalized))
+			return false;
 		if (messageType == null) {
-			if (other.messageType != null) {
+			if (other.messageType != null)
 				return false;
-			}
-		} else if (!messageType.equals(other.messageType)) {
+		} else if (!messageType.equals(other.messageType))
 			return false;
-		}
 		if (recipientUsers == null) {
-			if (other.recipientUsers != null) {
+			if (other.recipientUsers != null)
 				return false;
-			}
-		} else if (!recipientUsers.equals(other.recipientUsers)) {
+		} else if (!recipientUsers.equals(other.recipientUsers))
 			return false;
-		}
+		if (replied != other.replied)
+			return false;
+		if (replyDisabled != other.replyDisabled)
+			return false;
 		if (senderUser == null) {
-			if (other.senderUser != null) {
+			if (other.senderUser != null)
 				return false;
-			}
-		} else if (!senderUser.equals(other.senderUser)) {
+		} else if (!senderUser.equals(other.senderUser))
 			return false;
-		}
 		if (subject == null) {
-			if (other.subject != null) {
+			if (other.subject != null)
 				return false;
-			}
-		} else if (!subject.equals(other.subject)) {
+		} else if (!subject.equals(other.subject))
 			return false;
-		}
 		return true;
 	}
 
